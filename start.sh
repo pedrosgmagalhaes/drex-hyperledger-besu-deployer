b974e00fcb17bd79a642ceee6c77bafab0d27aa5
Pedro Magalhães <pedro@scrumlaunch.com>
2023-11-09 17:30:51 -0300

    updated validation nodes

--- start.sh
+++ start.sh
@@ -11,6 +11,7 @@ docker run -d -p 30303:30303 -p 30303:30303/udp -p 8545:8545 -p 8546:8546 \
   -e BESU_RPC_HTTP_HOST=0.0.0.0 \
   hyperledger/besu:latest \
   --genesis-file=/opt/besu/genesis.json \
+  --bootnodes="enode://daf4f0456e565c2d8f5cf44fca88dd42876058a231e3656ddd8644734d5fd373fe55787d2b1f26b3b8757ad8e3c990ed0ea20536625bffa1b571d90d2691e747@10.106.16.2:30303" \
   --rpc-http-port=8545 \
   --rpc-ws-port=8546 \
   --host-whitelist="*" \
