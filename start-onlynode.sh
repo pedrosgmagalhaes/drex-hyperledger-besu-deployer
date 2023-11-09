f11afb9d0202b668ac365faf98281fc59eb2fa18
Pedro Magalhães <pedro@scrumlaunch.com>
2023-11-09 18:50:35 -0300

    added privatekey

--- start-onlynode.sh
+++ start-onlynode.sh
@@ -4,7 +4,7 @@ docker run -d \
   hyperledger/besu:latest \
   --genesis-file=/opt/besu/genesis.json \
   --node-private-key-file=/opt/besu/key.priv \
-  --bootnodes="enode://b7f04229f180072a93067bcc6504ef52466aaa77a9f723272058220c0d1e2e8cd758c602b5d3b139c035ec4059ee97d65f04d9c70f9df5d10cc871bc7787458f@10.106.16.2:30303" \
+  --bootnodes="enode://1af64001226251d5e583b871f3cd56f5be75bab6c03792c63580105f2db832bcd50646fd66d7f4d3f4af0e40b9cdbb78ef1fcd90572c4052ada872afe872e7ba@10.106.16.2:30303" \
   --host-whitelist="*" \
   --rpc-http-host=0.0.0.0 \
   --rpc-ws-host=0.0.0.0
